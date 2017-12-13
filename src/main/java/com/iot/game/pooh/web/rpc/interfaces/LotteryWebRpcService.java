9c54085d3ea505fc119ec893e7023b9f0646344a
home45006 <home45006@126.com>
2017-12-13 11:26:34 +0800

    修改

--- src/main/java/com/iot/game/pooh/web/rpc/interfaces/LotteryWebRpcService.java
+++ src/main/java/com/iot/game/pooh/web/rpc/interfaces/LotteryWebRpcService.java
@@ -1,20 +1,32 @@
 package com.iot.game.pooh.web.rpc.interfaces;
 
 
+import com.iot.game.pooh.server.entity.json.enums.PoohAbnormalStatus;
+import com.iot.game.pooh.server.entity.json.enums.PoohNormalStatus;
+import com.iot.game.pooh.server.entity.rpc.RpcCommandResult;
+
 public interface LotteryWebRpcService {
 
 	/**
 	 * 投注开始(START)
 	 */
-	String startLottery(String roomId,String userId,String guessType);
+	RpcCommandResult startLottery(String roomId, String userName);
 
 	/**
 	 * 投注结束(CATCH)
 	 */
-	String endLottery(String roomId);
+	RpcCommandResult endLottery(String roomId, String userName);
 
 	/**
 	 * 开奖(FREE)
+	 *
+	 * @param gifinumber 抓中返回1 没抓中返回0
 	 */
-	String drawLottery(String roomId);
+	RpcCommandResult drawLottery(String roomId, Integer gifinumber);
+
+	/**
+	 * 异常通知
+	 */
+	String gatewayException(PoohNormalStatus poohNormalStatus, PoohAbnormalStatus poohAbnormalStatus);
 }
+
