6a654527faffc2e0be62ec0bcd52fac0bba95e4a
763 <763602352@qq.com>
2017-12-23 16:01:31 +0800

    序列化

--- src/main/java/com/iot/game/pooh/web/rpc/interfaces/entity/GuessDetail.java
+++ src/main/java/com/iot/game/pooh/web/rpc/interfaces/entity/GuessDetail.java
@@ -9,31 +9,11 @@ import java.io.Serializable;
  */
 @Data
 public class GuessDetail implements Serializable{
-    private Integer guessId;
-    private String appUserId;
-    private String dollId;
-    private String guessType;//最终竞猜结果
-    private String guessKey;//1中 0不中
-    private Integer guessGold;
-    private String createDate;
-    private String playbackId;
-    private String settlementFlag;//Y 清算 N 未清算
-    private String settlementDate;
+    private String nickname;//昵称
+    private String appUserId;//用户ID
 
     public GuessDetail(){
 
     }
 
-    public GuessDetail(String appUserId, String dollId, String guessKey, Integer guessGold, String playbackId) {
-        this.appUserId = appUserId;
-        this.dollId = dollId;
-        this.guessKey = guessKey;
-        this.guessGold = guessGold;
-        this.playbackId = playbackId;
-    }
-
-    public GuessDetail(String guessKey, String playbackId) {
-        this.guessKey = guessKey;
-        this.playbackId = playbackId;
-    }
 }
